f437481fc1ccba01c353bf495936e21046a9ae4b
Michelle L <mlawson@smith.edu>
2022-11-27 14:42:26 -0500

    updating world creation

--- World.java
+++ World.java
@@ -3,6 +3,7 @@ public class World {
     public int[] size;
     public Task[] tasks;
     public Building[] buildings;
+    //TODO: Make Building[] an ArrayList
 
     //Constructor
     public World(String name, int[] size){
@@ -11,6 +12,20 @@ public class World {
         this.tasks = new Task[5];
         this.buildings = new Building[10];
         System.out.println("The" + name+ " world has been created");
+    }
+
+    /**
+     * Create world Buildings
+     * @param building the building to be added to the world
+     */
+    public World addBuilding(Building building){
+        for (int i = 0; i < buildings.length; i++) {
+            if (buildings[i] == null) {
+                buildings[i] = building;
+                break;
+            }
+        }
+        return this;
     }
     
 }
